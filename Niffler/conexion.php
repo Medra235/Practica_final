8d8786cd1fb89ecde6a380c6c62660866a0692b0
Medra235 <102791570+Medra235@users.noreply.github.com>
2022-05-16 23:40:30 +0200

    Add files via upload

---
+++ Niffler/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+function conectar(){
+
+	$user="root";
+	$pass="";
+	$server="localhost";
+	$db="facilitophp";
+	$con=mysql_connect($server,$user,$pass) or die ("Error al conectar a la ase de datos".mysql_error());
+	mysql_select_db($db,$con);
+
+	return $con;
+}
