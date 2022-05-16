13b3c248881f2e09f259b932a352e262f93605ae
Medra235 <102791570+Medra235@users.noreply.github.com>
2022-05-16 23:59:24 +0200

    Update conexion.php

--- Niffler/conexion.php
+++ Niffler/conexion.php
@@ -4,8 +4,8 @@ function conectar(){
 
 	$user="root";
 	$pass="";
-	$server="localhost";
-	$db="facilitophp";
+	$server="donovan.ns.cloudflare.com";
+	$db="articulos";
 	$con=mysql_connect($server,$user,$pass) or die ("Error al conectar a la ase de datos".mysql_error());
 	mysql_select_db($db,$con);
 
